6d23a98576514677132afb85e918d68074b73f78
Jesse Roux <roux.jesse@gmail.com>
2018-05-03 17:25:18 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name="flask-routing",
+  version="1.0",
+  author="kangaroux",
+  author_email="roux.jesse@gmail.com",
+  packages=["flaskrouting"],
+  url="https://github.com/Kangaroux/flask-routing",
+  license="MIT",
+)
